164b03a91cd34932d987ed0de704d6509c2cdb49
jacksonflowers <flowersj@mit.edu>
2022-11-30 13:38:24 -0500

    Refactor generate_solution and add more tests

--- src/generate_solution.jl
+++ src/generate_solution.jl
@@ -3,7 +3,7 @@
 
 This function generates a solution to the problem using the current preferred method.
 """
-function generate_solution()
+function generate_solution(num_times=10, method=breadth_first)
     city = read_city()
-    return repeat(city)
+    return repeat(city, num_times, method)
 end
--- src/repeat.jl
+++ src/repeat.jl
@@ -3,7 +3,7 @@
 
 This function tries to find a good solution to the problem by repeatedly calling `method` and keeping the best solution found. This should result in improvements if `method` is randomized.
 """
-function repeat(city, method=breadth_first, num_times=10)
+function repeat(city, num_times, method)
     best_solution = method(city)
     best_distance = total_distance(best_solution, city)
     for i in 1:num_times
